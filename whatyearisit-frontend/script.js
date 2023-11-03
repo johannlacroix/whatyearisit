49457c064d692d0ba32e06ce290109ccbdbe1e9e
Joh <johann.lacroix@gmail.com>
2023-11-03 16:09:54 +0100

    whatyearisit

--- whatyearisit-frontend/script.js
+++ whatyearisit-frontend/script.js
@@ -1,3 +1,5 @@
-fetch('https://whatyearisit-backend-ochre.vercel.app/year').then(data => {
-    document.querySelector("#year").textContent = data.year
-})
+fetch('https://whatyearisit-backend-ochre.vercel.app/year')
+    .then(resp => resp.json())
+    .then(data => {
+        document.querySelector("#year").textContent = data.year
+    })
